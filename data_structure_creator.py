3302b8ac0e2da2b40757063124d8611ea25f950f
Mido Assran <midostuf@hotmail.com>
2017-02-05 02:03:06 -0500

    ADDED - Chi Squared

---
+++ data_structure_creator.py
@@ -0,0 +1,72 @@
+"""
+Data Structure Creation Script
+
+:description: Converts redit conversation data into fast-access data structures
+:author: Mido Assran
+:date: Feb. 4, 2017
+:note: Function names prepended with 'h_' are helper functions
+"""
+
+from collections import Counter
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+
+# ---------------------------------------------------------------------------- #
+# Script Parameters
+# ---------------------------------------------------------------------------- #
+DF_TRAIN_PATH = "train_input_clean.csv"
+DF_TARGET_PATH = "train_output.csv"
+DS_EXPORT_PATH = "data_structures.npz"
+# ---------------------------------------------------------------------------- #
+
+def main():
+    """ Calling code for the data structure creator """
+
+
+    # Create a single unified data frame with targets
+    dframe = pd.read_csv(DF_TRAIN_PATH)
+    dframe['category'] = pd.read_csv(DF_TARGET_PATH)['category']
+
+    # Tokenize all the conversations
+    dframe['tknzd_conversation'] = dframe['conversation'].apply(word_tokenize)
+
+    # Set of all categories in dframe
+    ctgry_count = Counter(list(dframe['category']))
+
+    # Set of the words in the dframe organized by category
+    wrd_counts_by_ctgry = {}
+    for ctgry in ctgry_count:
+        ctgry_dframe = dframe[dframe['category'] == ctgry]
+        cnvs = list(ctgry_dframe['tknzd_conversation'])
+        wrd_lst = [item for sublist in cnvs for item in sublist]
+        wrd_counts_by_ctgry[ctgry] = Counter(wrd_lst)
+
+    # Set of all words in dframe
+    wrd_count = sum(wrd_counts_by_ctgry.values(), Counter())
+
+    # Set of the categories in the dframe organized by words
+    ctgry_counts_by_wrd = {}
+    i, i_max = 0, len(wrd_count)
+    for wrd in wrd_count:
+        if i % 100 == 0:
+            print("{}%".format(round(i / i_max * 100, 3)), end='\r')
+        wrd_dframe = dframe[dframe['conversation'].str.contains(wrd)]
+        ctgrys = list(wrd_dframe['category'])
+        ctgry_counts_by_wrd[wrd] = Counter(ctgrys)
+        i += 1
+
+    # Export results to a npz compressed file
+    np.savez_compressed(DS_EXPORT_PATH,
+                        wrd_count=wrd_count,
+                        ctgry_count=ctgry_count,
+                        ctgry_counts_by_wrd=ctgry_counts_by_wrd,
+                        wrd_counts_by_ctgry=wrd_counts_by_ctgry)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
--- visualizer.py
+++ visualizer.py
@@ -7,93 +7,108 @@ Visualizer.py
 :note: Function names prepended with 'h_' are helper functions
 """
 
+import numpy as np
 from collections import Counter
-import pandas as pd
-from nltk.tokenize import word_tokenize
 
 # ---------------------------------------------------------------------------- #
 # Script Parameters
 # ---------------------------------------------------------------------------- #
-DF_TRAIN_PATH = "train_input_clean.csv"
-DF_TARGET_PATH = "train_output.csv"
+DS_LOAD_PATH = "data_structures.npz"
 # ---------------------------------------------------------------------------- #
 
-def h_category_counts(dframe, ctgry_set):
+def h_chi_squared_metric(wrd_counts_by_ctgry, ctgry_counts_by_wrd, ctgry_count,
+                         term, category):
     """
-    <Helper> Determine the number of instances of each category in the dframe
+    <HELPER> Compute the chi_squared_metric between 'term' and 'category'
 
-    :type dframe: pandas.dframe
-    :type ctgry_set: set(str)
-    :rtype: dict("category": count)
-    """
-    ctgry_counts = {}
-    for ctgry in ctgry_set:
-        ctgry_counts[ctgry] = dframe[dframe['category'] == ctgry].count()[0]
-    return ctgry_counts
+    :ref-paper: Yang97acomparative
 
-def h_word_counts_by_category(ctgry_set, wrd_lst_by_ctgry):
-    """
-    <Helper> Determine the number of words in each category of the dframe
-
-    :type ctgry_set: set(str)
-    :type wrd_lst_by_ctgry: dict("category": list(str))
-    :rtype: dict("category": dict("word": count))
-    """
-    counts = {}
-    for ctgry in ctgry_set:
-        wrd_lst = wrd_lst_by_ctgry[ctgry]
-        counts[ctgry] = Counter(wrd_lst)
-    return counts
-
-def frequency_metric(ctgry_counts, wrd_counts, top_n=4):
-    """
-    Visualize the frequency of the 'top_n' most common words in each category
-
+    :type wrd_counts_by_ctgry: dict('category': Counter('word': count))
+    :type ctgry_counts_by_wrd: dict('word': Counter('category': count))
+    :type ctgry_count: Counter('category': count)
     :type top_n: int (=DEFAULT:4)
+    :type bottom_n: int (=DEFAULT:4)
+    :rtype: float
+    """
+    # t is the term, c the category
+    t, c = term, category
+    # N is the total number of documents
+    N = sum(ctgry_count.values())
+    # A is the number of times t and c co-occur
+    A = wrd_counts_by_ctgry[c][t]
+    # B is the number of times t occurs without c
+    B = sum(ctgry_counts_by_wrd[t].values()) - A
+    # C is the number of times c occurs without t
+    C = sum(wrd_counts_by_ctgry[c].values()) - A
+    # D is the number of times that neither c nor t occur
+    D = N - (A + B + C)
+    # Return the chi_squared_metric between 'term' and 'category'
+    return N * (A * D - C * B) ** 2 / ((A + C) * (B + D) * (A + B) * (C + D))
+
+def visualize_chi_squared_metric(wrd_counts_by_ctgry, ctgry_counts_by_wrd,
+                                 ctgry_count, term):
+    """
+    Compute and visualize the chi_squared_metric between 'term' & all categories
+
+    :type wrd_counts_by_ctgry: dict('category': Counter('word': count))
+    :type ctgry_counts_by_wrd: dict('word': Counter('category': count))
+    :type ctgry_count: Counter('category': count)
+    :type top_n: int (=DEFAULT:4)
+    :type bottom_n: int (=DEFAULT:4)
     :rtype: void
     """
-    print("Category frequencies:", ctgry_counts)
+    chi_sqrd = []
+    for ctgry in ctgry_count:
+        chi_sqrd.append(
+            h_chi_squared_metric(wrd_counts_by_ctgry=wrd_counts_by_ctgry,
+                                 ctgry_count=ctgry_count,
+                                 ctgry_counts_by_wrd=ctgry_counts_by_wrd,
+                                 term=term, category=ctgry)
+        )
+    avg_chi_sqrd = sum(chi_sqrd) / float(len(chi_sqrd))
+    print("Chi Squared Metric (", term, ") :", avg_chi_sqrd)
+
+def visualize_frequency_metric(ctgry_counts, wrd_counts_by_ctgry,
+                               top_n=4, bottom_n=4):
+    """
+    Visualize the 'top_n' & 'bottom_n' most common words in each category
+
+    :type ctgry_count: Counter('category': int)
+    :type wrd_counts_by_ctgry: dict('category': Counter('word': count))
+    :type top_n: int (=DEFAULT:4)
+    :type bottom_n: int (=DEFAULT:4)
+    :rtype: void
+    """
+    print("Category frequencies:", ctgry_counts, end="\n\n")
     print("Word Frequencies:")
     for ctgry in ctgry_counts:
-        print("\t", ctgry, ":", wrd_counts[ctgry].most_common(top_n))
+        print("\t", ctgry, ":",
+              wrd_counts_by_ctgry[ctgry].most_common(top_n),
+              wrd_counts_by_ctgry[ctgry].most_common()[-bottom_n:])
 
 def main():
     """ Calling code for the visualizer """
 
-    # ------------------------------------------ #
-    # Pre-calculations
-    # ------------------------------------------ #
-    # Create a single unified data frame with targets
-    dframe = pd.read_csv(DF_TRAIN_PATH)
-    dframe['category'] = pd.read_csv(DF_TARGET_PATH)['category']
+    data = np.load(DS_LOAD_PATH)
+    ctgry_count = data['ctgry_count'].item()
+    # wrd_count = data['wrd_count'].item() # Unused for now
+    wrd_counts_by_ctgry = data['wrd_counts_by_ctgry'].item()
+    ctgry_counts_by_wrd = data['ctgry_counts_by_wrd'].item()
 
-    # Data structure of the categories in the dframe
-    ctgry_set = set(list(dframe['category']))
-
-    # Data structure of the words in each category in the dframe
-    wrd_lst_by_ctgry = {}
-    for ctgry in ctgry_set:
-        ctgry_dframe = dframe[dframe['category'] == ctgry]
-        cnvs = list(ctgry_dframe['conversation'])
-        wrd_lst = []
-        for cnv in cnvs:
-            wrd_lst += word_tokenize(cnv)
-        wrd_lst_by_ctgry[ctgry] = wrd_lst
-
-    # Distribution of categories
-    ctgry_counts = h_category_counts(dframe, ctgry_set)
-    # Distribution of words by category
-    wrd_counts = h_word_counts_by_category(ctgry_set, wrd_lst_by_ctgry)
-    # ------------------------------------------ #
 
     # Visualize word frequencies in each category
-    frequency_metric(ctgry_counts, wrd_counts, top_n=6)
+    visualize_frequency_metric(ctgry_count, wrd_counts_by_ctgry,
+                               top_n=2, bottom_n=2)
 
     # Compute the information gain
 
-
+    # Wrd is an example of the term for which we want to compute chi-squared
+    wrd = "goal"
     # Compute the chi squared metric of each word relative to each category
-
+    visualize_chi_squared_metric(wrd_counts_by_ctgry=wrd_counts_by_ctgry,
+                                 ctgry_count=ctgry_count,
+                                 ctgry_counts_by_wrd=ctgry_counts_by_wrd,
+                                 term=wrd)
 
 
 if __name__ == "__main__":
